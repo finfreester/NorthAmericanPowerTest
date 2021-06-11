f5d9d224166ed24228f151b6e0dd85594a54bd29
Pete Rodriguez <pete@codebridgesoftware.com>
2021-06-10 22:35:21 -0400

    initial feature

--- NorthAmericanPowerDomain/Models/UserAccount.cs
+++ NorthAmericanPowerDomain/Models/UserAccount.cs
@@ -30,6 +30,8 @@ namespace NorthAmericanPower.Domain
         [EmailAddress, Required]
         public string Email { get; set; }
 
+        public string newProp { get; set; }
+
         public int AnotherProp { get; set; }
     }
 }
